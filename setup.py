e7413ba44aae0091421d0e0b791403cca36fcc3d
GalSajko <gal.sajko@gmail.com>
2023-05-30 10:47:16 +0200

    Setup file.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="gwpoffpm",
+    packages=find_packages(include=["gwpoffpm"]),
+    version="1.0.0",
+    description="Green-Wall-Project package for prediction of leg-movement offsets.",
+    author="Benjamin Fele, Gal Sajko",
+    license="MIT",
+)
